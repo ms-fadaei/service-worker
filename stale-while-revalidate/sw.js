f08fcdb6edef1c23cd350826260f893750c1a45f
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2021-10-05 22:22:42 +0330

    feat: add stale-while-revalidate stratrgy

---
+++ stale-while-revalidate/sw.js
@@ -0,0 +1,56 @@
+const NETWORK_TIMEOUT = 100;
+const CACHE_VERSION = 1;
+const CASH_NAME = `swr-${CACHE_VERSION}`;
+const IMAGE_REPO_LIST = [
+    "/assets/images/img-1.jpeg",
+];
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(precache());
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("fetch", function (event) {
+    console.log("Request to:", event.request.url);
+
+    event.respondWith(fromCache(event.request).catch(() => fetch(event.request)));
+    event.waitUntil(stealWhileRevalidate(event.request));
+})
+
+function precache() {
+    return caches.open(CASH_NAME)
+        .then(res => res.addAll(IMAGE_REPO_LIST))
+        .catch(err => console.error("An error occurred on precaching!", err));
+}
+
+function fromCache(request) {
+    return caches.open(CASH_NAME)
+        .then(function (cache) {
+            return cache.match(request)
+                .then(function (response) {
+                    if (!response) return Promise.reject("no-cache");
+
+                    console.log(`Response for the "${request.url}" served from cache!`);
+                    return response;
+                });
+        })
+}
+
+let i = 1;
+
+function stealWhileRevalidate(request) {
+    if (request.url.includes("/assets/images/img-")) {
+        var url = `/assets/images/img-${i++ % 5 + 1}.jpeg`;
+    } else {
+        return Promise.resolve();
+    }
+
+    return fetch(url)
+        .then(function (response) {
+            return caches.open(CASH_NAME)
+                .then(cache => {
+                    console.log("Cache revalidated", request.url);
+                    return cache.put(request, response);
+                });
+        });
+}
