15b04e9da40ddf0fe3ac375d4f664aab75bdf3d3
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2021-10-06 20:09:34 +0330

    chore: update texts

--- stale-while-revalidate-then-update/index.js
+++ stale-while-revalidate-then-update/index.js
@@ -1,5 +1,5 @@
 const CACHE_VERSION = 1;
-const CASH_NAME = `swr-and-update-${CACHE_VERSION}`;
+const CASH_NAME = `swr-then-update-${CACHE_VERSION}`;
 
 if ("serviceWorker" in navigator) {
   const notice = document.getElementById("update-notice");
